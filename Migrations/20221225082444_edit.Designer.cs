38efb7d0b44be5e10eb669a7458d1c37948ea4fe
garaev03 <5mptrbf@code.edu.az>
2023-01-05 08:23:46 +0400

    done

--- Migrations/20221225082444_edit.Designer.cs
+++ Migrations/20221225082444_edit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MultiShop.DAL;
 
@@ -11,9 +12,10 @@ using MultiShop.DAL;
 namespace MultiShop.Migrations
 {
     [DbContext(typeof(MultiShopDbContext))]
-    partial class MultiShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221225082444_edit")]
+    partial class edit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
